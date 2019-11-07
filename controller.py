c9acba8a2f38e5c5d776683dd1962d70c5e66af9
birkin <birkin.diana@gmail.com>
2019-11-07 11:58:44 -0500

    stubs async item-api calls.

--- controller.py
+++ controller.py
@@ -1,5 +1,6 @@
 import datetime, json, logging, os, pprint, sys
-import requests
+import asks, requests, trio
+from asks import BasicAuth as asksBasicAuth
 from requests.auth import HTTPBasicAuth
 
 
@@ -35,10 +36,11 @@ def get_token():
     return token
 
 
-def save_initial_downloads( jsn_str ):
+def save_initial_downloads( jsn_str, counter ):
     """ Saves raw json files from initial json-query calls.
         Called by run_json_query() """
-    filename= '%s/a_initial_downloads/%s.json' % ( FILE_DOWNLOAD_DIR, str(datetime.datetime.now()).replace(' ', '_') )
+    counter_str = f'{counter:03}'
+    filename= '%s/a_initial_downloads/%s.json' % ( FILE_DOWNLOAD_DIR, counter_str )
     with open( filename, 'wb' ) as f:
         f.write( jsn_str )
     return
@@ -47,6 +49,7 @@ def save_initial_downloads( jsn_str ):
 def run_json_query():
     """ Runs the json query and saves all files. """
     log.debug( '\n-------\nrunning the json query' )
+    start_time = datetime.datetime.now()
     token = get_token()
     query_json = '''[
         [
@@ -118,13 +121,15 @@ def run_json_query():
     limit = 100
     continue_flag = True
     custom_headers = {'Authorization': 'Bearer %s' % token }
+    counter = 1
     while continue_flag is True:
         items_query_url = f'{API_ROOT_URL}items/query?offset={offset}&limit={limit}'
         payload = { 'json': query_json }
         r = requests.post( items_query_url, headers=custom_headers, json=payload )
         log.debug( f'url was, ```{r.url}```' )
-        save_initial_downloads( r.content )
+        save_initial_downloads( r.content, counter )
         offset = offset + limit
+        counter += 1
         # if offset > 10000000:  # this is the REAL CODE
         #     continue_flag = False
         # rsp_dct = r.json()
@@ -132,11 +137,140 @@ def run_json_query():
         #     continue_flag = False
         if offset > 200:  # TESTING -- to save 3 files of 100 each
             continue_flag = False
+    time_taken = datetime.datetime.now() - start_time
+    log.debug( f'run_json_query time_taken, ```{time_taken}```' )
 
     ## end def run_json_query()
 
 
-def get_bibs():
+def make_items_dcts():
+    """ For each json-query file, prepares a dict with key of item-id. """
+    start_time = datetime.datetime.now()
+    json_downloads_dir = f'{FILE_DOWNLOAD_DIR}/a_initial_downloads'
+    items_dct_dir = f'{FILE_DOWNLOAD_DIR}/b_items_dct'
+    counter = 1
+    for jfile in os.listdir( json_downloads_dir ):
+        log.debug( f'processing file, ```{jfile}```' )
+        counter_str = f'{counter:03}'
+        if jfile.endswith( '.json' ):
+            entries_filepath = f'{json_downloads_dir}/{jfile}'
+            log.debug( f'entries_filepath, ```{entries_filepath}```' )
+            entries_dct = {}
+            items_dct = {}
+            with open( entries_filepath, 'r' ) as f:
+                entries_dct = json.loads( f.read() )
+            for entry_dct in entries_dct['entries']:
+                url = entry_dct['link']
+                item_id = url[-8:]
+                items_dct[item_id] = { 'link': url, 'item_dct': None, 'bib_dct': None }
+            items_filepath = f'{items_dct_dir}/{counter_str}.json'
+            log.debug( f'about to write out item-dct-file, ```{items_filepath}```' )
+            with open( items_filepath, 'w' ) as f2:
+                jstring = json.dumps( items_dct, sort_keys=True, indent=2 )
+                f2.write( jstring )
+            counter +=1
+    time_taken = datetime.datetime.now() - start_time
+    log.debug( f'make_items_dcts time_taken, ```{time_taken}```' )
+    return
+
+
+# async def get_item_data():
+#     start_time = datetime.datetime.now()
+#     auth_token = get_token()
+#     custom_headers = {'Authorization': f'Bearer {auth_token}' }
+#     url = 'https://catalog.library.brown.edu/iii/sierra-api/v5/items/10000001'
+#     rsp = await asks.get( url, headers=custom_headers, timeout=2 )
+#     log.debug( f'output, ```{rsp.json()}```' )
+#     time_taken = datetime.datetime.now() - start_time
+#     log.debug( f'get_item_data time_taken, ```{time_taken}```' )
+#     return
+
+
+async def fetch_item_data( index_key, custom_headers, results_holder_dct ):
+    url = 'https://httpbin.org/delay/1'
+    rsp = await asks.get( url, headers=custom_headers, timeout=2 )
+    results_holder_dct[ str(index_key) ] = rsp.status_code
+    log.debug( 'fetch done' )
+    return
+
+async def get_item_data():
+    """ Populates each item-dct with item-data, and saves file. """
+    start_time = datetime.datetime.now()
+    auth_token = get_token()
+    custom_headers = {'Authorization': f'Bearer {auth_token}' }
+    source_dir = f'{FILE_DOWNLOAD_DIR}/b_items_dct'
+    destination_dir = f'{FILE_DOWNLOAD_DIR}/c_items_dct'
+    for source_file in os.listdir( source_dir ):
+        if source_file.endswith( '.json' ):
+            source_filepath = f'{source_dir}/{source_file}'
+            source_dct = {}
+            with open( source_filepath, 'r' ) as f:
+                source_dct = json.loads( f.read() )
+
+        key_lst = list( source_dct.keys() )
+        # log.debug( f'key_lst, ```{key_lst}```' )
+
+        key_count = len( key_lst )
+        key_count = 10  # TEMP!!
+        worker_count = 3
+
+        ( range_count, extra ) = divmod( len(key_lst), worker_count )
+        log.debug( f'range_count, `{range_count}`; extra, `{extra}`' )
+
+        ## <https://stackoverflow.com/a/1335618>
+        # zip(*(iter(range(10)),) * 3)
+        # map( None, *(iter(range(10)),) * 3 )
+
+        # ranges = list( zip(*(iter(range(len(key_lst))),) * worker_count) )  # output: ```[(0, 1, 2), ... (96, 97, 98)]```
+        ranges = list( zip(*(iter(range(key_count)),) * worker_count) )  # output: ```[(0, 1, 2), ... (96, 97, 98)]```
+        log.debug( f'ranges, ```{ranges}```' )
+
+        range_values = []
+        for sub_range in ranges:
+            sub_range_values = []
+            for index in sub_range:
+                value = key_lst[index]
+                sub_range_values.append( value )
+            range_values.append( sub_range_values )
+        log.debug( f'range_values, ```{range_values}```' )
+
+        results_holder_dct = {}
+        for sub_range in range_values:
+            async with trio.open_nursery() as nursery:
+                for index in sub_range:
+                    log.debug( f'index, `{index}' )
+                    nursery.start_soon( fetch_item_data, index, custom_headers, results_holder_dct )
+            log.debug( f'end of sub_range, results_holder_dct, ```{results_holder_dct}```' )
+            if extra > 0:
+                async with trio.open_nursery() as nursery:
+                    for i in range( extra ):
+                        nursery.start_soon( fetch_item_data, index, custom_headers, results_holder_dct )
+
+
+        break
+
+
+        # async with trio.open_nursery() as nursery:
+        #     results_holder_dct = {}
+        #     for i, (key, val_dct) in enumerate( source_dct.items() ):
+        #         nursery.start_soon( grab_item_data, key, results_holder_dct )
+
+        #         # nursery.start_soon( self.fetch_945_data, results_holder_dct )
+        #         # nursery.start_soon( self.fetch_sierra_token, results_holder_dct )  # NOTE: after getting token, fetch_sierra_token() will set up its own nursery to call the bib-api and the items-api.
+
+        #         log.debug( f'i, `{i}`' )
+        #         log.debug( f'key, `{key}`' )
+        #         log.debug( f'val_dct, ```{val_dct}```' )
+        #         log.debug( 'nursery start called')
+        #     if i
+
+
+
+    # url = 'https://catalog.library.brown.edu/iii/sierra-api/v5/items/10000001'
+    # rsp = await asks.get( url, headers=custom_headers, timeout=2 )
+    # log.debug( f'output, ```{rsp.json()}```' )
+    time_taken = datetime.datetime.now() - start_time
+    log.debug( f'get_item_data time_taken, ```{time_taken}```' )
     return
 
 
@@ -145,7 +279,9 @@ if __name__ == '__main__':
     log.debug( f'argument, `{arg}`' )
     if arg == 'run_json_query':
         run_json_query()
-    elif arg == 'get_bibs':
-        get_bibs()
+    elif arg == 'make_items_dct':
+        make_items_dcts()
+    elif arg == 'get_item_data':
+        trio.run( get_item_data )
 
 
